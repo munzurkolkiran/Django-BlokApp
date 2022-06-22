0ec77eddf4d3abd0a135dba524686135ea9945bd
Munzur Kolkiran <munzur56@gmail.com>
2022-06-22 15:52:52 +0300

    Kategori modeli olusturuldu.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from blog.models import KatagoriModel
 
-# Register your models here.
+admin.site.register(KatagoriModel)
---
+++ blog/models/__init__.py
@@ -0,0 +1,1 @@
+from .katagori import KatagoriModel
