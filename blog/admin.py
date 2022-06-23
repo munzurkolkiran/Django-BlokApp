f2a292119bfadcf8d6d0710f0c7db70a6ef8e63e
Munzur Kolkiran <munzur56@gmail.com>
2022-06-23 08:30:15 +0300

    yazi admin paneli duzenlendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from blog.models import KategoriModel
+from blog.models import YazilarModel
 
 admin.site.register(KategoriModel)
+
+
+class YazilarAdmin(admin.ModelAdmin):
+    search_fields = ('baslik', 'icerik')
+    list_display = (
+        'baslik', 'olusturulma_tarihi', 'duzenlenme_tarihi'
+    )
+
+
+admin.site.register(YazilarModel, YazilarAdmin)
