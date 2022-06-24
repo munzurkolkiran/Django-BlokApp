6ed99076403701eb960f3f3e66debab8c6a7bb50
Munzur Kolkiran <munzur56@gmail.com>
2022-06-24 10:26:55 +0300

    Paginations islemleri yapildi

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from blog.views import iletisim, anasayfa
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
-    path('anasayfa/', anasayfa, name='anasayfa'),
     path('iletisim/', iletisim, name='iletisim')
 ]
--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from blog.models import YazilarModel
+from django.core.paginator import Paginator
 
 
 def anasayfa(request):
-    context = {
-        'isim': 'munzur kolkiran'
-    }
+    yazilar = YazilarModel.objects.all().order_by('-guncelleme_tarihi')
+
+    sayf = request.GET.get('sayf')
+    paginator = Paginator(yazilar, 2)
+
+    context = {'yazilar': paginator.get_page(sayf)}
     return render(request, 'pages/anasayfa.html', context)
